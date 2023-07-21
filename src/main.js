b92f6dbe8c1b55d628a2a220e4283843677f1d48
GracielaFaz <graciefaz@gmail.com>
2023-07-20 18:21:20 -0600

    Ruta register

--- src/main.js
+++ src/main.js
@@ -1,34 +1,39 @@
 // Este es el punto de entrada de tu aplicación
 
-import { myFunction } from './lib/index.js';
+/* import { myFunction } from './lib/index.js';
 
-myFunction();
+myFunction(); */
 
 import { home } from './components/home.js';
 import { login } from './components/login.js';
+import { register } from './components/register.js';
+import { register2 } from './components/register2.js';
 
 // Llamamos a la función home y la agregamos al DOM
-const root = document.getElementById("root");
 
-const routes = [  
-    { path: "/", components: home },
-    { path: "/login", components: login },
+const root = document.getElementById('root');
+
+const routes = [
+  { path: '/', components: home },
+  { path: '/login', components: login },
+  { path: '/register', components: register },
+  { path: ' /register2', components: register2 },
 ];
 
-const defaultRoute = "/";
+const defaultRoute = '/';
 
 function navigateTo(hash) {
-    const route = routes.find((routeFind) => routeFind.path === hash); 
+  const route = routes.find((routeFind) => routeFind.path === hash);
 
-    if (route && route.components) {
-        window.history.pushState(
-            {},
-            route.path,
-            window.location.origin + route.path
-        );
-        root.appendChild(route.components());
-    }
+  if (route && route.components) {
+    window.history.pushState(
+      {},
+      route.path,
+      window.location.origin + route.path,
+    );
+    root.appendChild(route.components());
+  }
 }
 
-navigateTo(window.location.pathname);
-
+// puse el ||defaultRoute pq me marcaba error y no me dejaba hacer el commit//
+navigateTo(window.location.pathname || defaultRoute);
