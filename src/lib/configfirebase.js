8a7e735f25975c5d3c5eaa5537f950205cd67614
Diana Morales <dianamorales100599@gmail.com>
2023-07-26 13:45:35 -0400

    IniciodeFirebase

---
+++ src/lib/configfirebase.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyChEC862gwvNlWKnkWUBRsgXPdbjev_5R8",
+    authDomain: "socialnetworksn3.firebaseapp.com",
+    projectId: "socialnetworksn3",
+    storageBucket: "socialnetworksn3.appspot.com",
+    messagingSenderId: "861707856095",
+    appId: "1:861707856095:web:59637a3bf74467ac09e16a",
+    measurementId: "G-3GSEZ28NWC"
+  };
