37d08a5f0c889887c74d784c73f7c1c0bd074374
Mariaorostegui <mfop001new@gmail.com>
2023-08-01 18:54:25 -0500

    first view incomplete

--- src/components/home.js
+++ src/components/home.js
@@ -1,15 +1,42 @@
 function home(navigateTo) {
   const section = document.createElement('section');
+  const header = document.createElement('header');
+  const logo = document.createElement('img');
   const title = document.createElement('h2');
+  const message = document.createElement('p');
+  const square = document.createElement('div');
+  const containerButton = document.createElement('div');
   const button = document.createElement('button');
+  const buttonRegister = document.createElement('button');
+
+  title.textContent = 'Travel with Your Pet';
+  message.textContent = 'With just one click you\'ll have all the information you need';
   button.textContent = 'Login';
+  buttonRegister.textContent = 'Sign Up';
+
+  section.classList.add('first-view');
+  containerButton.classList.add('container-button');
+  buttonRegister.classList.add('button');
+  button.classList.add('button-second-style');
+  square.classList.add('square');
+  title.classList.add('title');
+  message.classList.add('message');
+  header.classList.add('logo');
+
   button.addEventListener('click', () => {
     navigateTo('/login');
   });
 
-  title.textContent = 'Welcome to Pawssport 😺';
+  buttonRegister.addEventListener('click', () => {
+    navigateTo('/register');
+  });
 
-  section.append(title, button);
+  logo.setAttribute('src', './img/logo.png');
+  logo.classList.add('logo');
+
+  header.appendChild(logo);
+  containerButton.append(button, buttonRegister);
+  section.append(header, title, message, square, containerButton);
   return section;
 }
 
