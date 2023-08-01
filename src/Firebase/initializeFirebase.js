c363bcf5b62d5f266c8988fc11c22f103ea715ba
NahomiVRojas <nahomivillanueva2@gmail.com>
2023-08-01 16:55:15 -0500

    Configuraciones Firebase

---
+++ src/Firebase/initializeFirebase.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyBQSjXUZbjydhVPpxNH2mzsAJqLNrK5qoU",
+    authDomain: "spookyverse-15191.firebaseapp.com",
+    projectId: "spookyverse-15191",
+    storageBucket: "spookyverse-15191.appspot.com",
+    messagingSenderId: "175001691307",
+    appId: "1:175001691307:web:745ec01b177ed0666e03ee",
+    measurementId: "G-G641G3M4D1"
+  };
