53df8cac78161fd1e8ce96dbeb55006dbfcce0e8
Natali <natalic1008.nm@gmail.com>
2023-08-01 12:53:36 -0500

    Vista Home -CSS

--- src/components/home.js
+++ src/components/home.js
@@ -1,16 +1,28 @@
 function home (navigateTo){
+    const contenedorGeneral = document.createElement ('div');
+    contenedorGeneral.className='contenedor'
+    contenedorGeneral.textContent = 'hola'
     const section = document.createElement('section'); 
     const title = document.createElement('h2');
-    const button = document.createElement('button');
+    const buttonInicio = document.createElement('button');
+    const buttonRegistro =document.createElement('button');
     
-    button.textContent = 'login'
-    button.addEventListener('click', () => {
+    buttonInicio.textContent = 'Iniciar sección'
+    buttonInicio.addEventListener('click', () => {
         navigateTo('/login');
     });
     
+
+    buttonRegistro.textContent = 'Registrate'
+    buttonRegistro.addEventListener('click', () => {
+        navigateTo('/registro');
+    });
+
     title.textContent = 'BIENVENIDOS A BON-BON';
-    section.append(title, button);
-    return section;
+
+
+   contenedorGeneral.append(section,title, buttonInicio, buttonRegistro);
+    return contenedorGeneral;
 }
 
 export default home;
