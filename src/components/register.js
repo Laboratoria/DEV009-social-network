347c77bf11016e62424373e084f6d2ab3f3d9821
jariannat <jaripache01@gmail.com>
2023-08-02 18:29:47 -0500

    lol

--- src/components/register.js
+++ src/components/register.js
@@ -46,51 +46,8 @@ function register(navigateTo) {
             errorMessage.textContent = 'Por favor, ingresa un correo y una contraseña válida.';
         }
     });
-  const section = document.createElement('section');
-  const buttonRegister = document.createElement('button');
-  const buttonGoogle = document.createElement('button');
-  const password = document.createElement('input');
-  const name = document.createElement('input');
-  const email = document.createElement('input');
-  const logo = document.createElement('img');
-  const avatar = document.createElement('img');
-  const errorMessage = document.createElement('p');
-  const successMessage = document.createElement('p');
-
-  logo.src = './imagenes/image.png';
-  avatar.src = './imagenes/avatar.png';
-  avatar.classList.add('avatar');
-  buttonRegister.textContent = 'Registrate';
-  buttonGoogle.textContent = 'Continuar con Google';
-  password.placeholder = 'Crea tu Contraseña';
-  name.placeholder = 'Ingresa tu nombre';
-  email.placeholder = 'Ingresa tu Email';
-  errorMessage.style.color = 'red';
-  successMessage.style.color = 'green';
-
-  buttonRegister.addEventListener('click', async () => {
-    const userEmail = email.value;
-    const userPassword = password.value;
-    if (userEmail && userPassword) {
-      try {
-        const user = await createUser(userEmail, userPassword);
-        successMessage.textContent = 'Usuario registrado con éxito';
-        errorMessage.textContent = '';
-        navigateTo('/feed');
-      } catch (error) {
-        errorMessage.textContent = error.message;
-        successMessage.textContent = '';
-      }
-    } else {
-      errorMessage.textContent = 'Por favor, ingresa un correo y una contraseña válida.';
-    }
-  });
-
-  /* */
-
-  password.addEventListener('input', () => {
-    password.type = 'password';
-  });
+  
+  };
 
   buttonGoogle.addEventListener('click', async () => {
     try {
@@ -102,5 +59,4 @@ function register(navigateTo) {
   });
   section.append(logo, avatar, name, email, password, buttonRegister, buttonGoogle, errorMessage, successMessage/* logoutButtom */);
   return section;
-}
 export default register;
