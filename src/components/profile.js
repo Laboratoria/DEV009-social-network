c2a808b2f5af9dbd199b851e375c86ec0587ab6c
Nancy A Contreras <nancyalejandra24mv@gmail.com>
2023-08-02 13:23:15 -0700

    vistas y router terminados

---
+++ src/components/profile.js
@@ -0,0 +1,53 @@
+function profile(navigateTo) {
+    
+    const mainConteiner = document.createElement('section');
+    const section = document.createElement('section');
+    const header = document.createElement('header');
+    const logo = document.createElement('img');
+    const profilePic = document.createElement('img');
+    const createPost = document.createElement('input');
+
+    const post = document.createElement('div');
+    const userName = document.createElement('h3');
+    const editPost = document.createElement('img');
+    const deletePost = document.createElement('img');
+    const postContent = document.createElement('p');
+    const like = document.createElement('img');
+    const countLikes = document.createElement('p');
+
+    const menu = document.createElement('nav');
+    const home = document.createElement('img');
+    const profile = document.createElement('img');
+    const exit = document.createElement('img');
+
+    logo.src = "./img/logo.png";
+    profilePic.src = "./img/perfil.png";
+    createPost.placeholder = "¿Cuál fue el ultimo lugar que visitaste?";
+
+    editPost.src = "./icons/edit.svg";
+    deletePost.src = "./icons/delete.svg";
+    like.src = "./icons/plane.svg";
+    countLikes.textContent = "0";
+    home.src = "./icons/home.svg";
+    exit.src = "./icons/exit.svg";
+
+    editPost.addEventListener('click', () => {
+        // editar post
+
+    });
+
+    deletePost.addEventListener('click', () => {
+        // eliminar post
+
+    });
+
+    mainConteiner.append(section, post, header, menu);
+    section.append(header, profilePic, createPost, post, menu);
+    post.append(userName, editPost, deletePost, postContent, like, countLikes);
+    header.appendChild(logo);
+    menu.append(home, profile, exit);
+
+    return mainConteiner;
+}
+
+export default profile;
---
+++ src/components/signup.js
@@ -0,0 +1,43 @@
+function signup(navigateTo){
+
+const section = document.createElement('section');
+const logo = document.createElement('img');
+const title = document.createElement('h2');
+title.textContent = "Registrate";
+
+const name = document.createElement('input');
+const email = document.createElement('input');
+const password = document.createElement('input');
+const validatePassword = document.createElement('input');
+const btnSignup = document.createElement('button');
+const toLogin = document.createElement('p');
+const or = document.createElement('p');
+const signupWithGmail =  document.createElement('a');
+const linkGmail =  document.createElement('img');
+
+logo.src = "./img/logo.png";
+name.placeholder = "Nombre";
+email.placeholder = "ejemplo@correo.com";
+password.placeholder = "Contraseña";
+validatePassword.placeholder = "Confirmar contraseña";
+
+btnSignup.textContent = "Registrarse";
+toLogin.textContent = "Ya tengo una cuenta";
+or.textContent = "ó";
+signupWithGmail.textContent = "Registrarse con";
+linkGmail.src = "./icons/gmail.svg";
+linkGmail.width = 24;
+linkGmail.height = 24;
+
+btnSignup.addEventListener('click',()=>{
+    navigateTo("/wall");
+})
+toLogin.addEventListener('click', ()=>{
+    navigateTo("/");
+} )
+section.append(logo, title, name, email, password, validatePassword, btnSignup, toLogin, or, signupWithGmail, linkGmail);
+return section;
+}
+
+export default signup;
+
