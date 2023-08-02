4cade2f62f55492f9207b6b22f66d0fae2a4a767
oolidu <arquitecturaod@gmail.com>
2023-08-02 13:48:09 -0400

    Avance 1 Firebase

--- src/main.js
+++ src/main.js
@@ -1,39 +1,45 @@
+import { registrarUsuario } from './lib/index.js';
+
 import home from './components/home.js';
 import login from './components/login.js';
 import error from './components/error.js';
 
+const email = 'usuario@gmail.com';
+const pass = '123456789';
+
+registrarUsuario(email, pass);
+
 const root = document.getElementById('root');
 
 const routes = [
-    { path: '/', component: home },
-    { path: '/login', component: login },
-    { path: '/error', component: error },
-  ];
+  { path: '/', component: home },
+  { path: '/login', component: login },
+  { path: '/error', component: error },
+];
 
-const defaultRoute = "/";
+const defaultRoute = '/';
 
 function navigateTo(hash) {
-    const route = routes.find((routeFind) => routeFind.path === hash);
+  const route = routes.find((routeFind) => routeFind.path === hash);
 
-    if (route && route.component) {
-        window.history.pushState(
-          {},
-          route.path,
-          window.location.origin + route.path,
-        );
+  if (route && route.component) {
+    window.history.pushState(
+      {},
+      route.path,
+      window.location.origin + route.path,
+    );
     if (root.firstChild) {
-        root.removeChild(root.firstChild);
-      }
-      root.appendChild(route.component(navigateTo));
-    } else {
-            navigateTo('/error');    
-        }
+      root.removeChild(root.firstChild);
+    }
+    root.appendChild(route.component(navigateTo));
+  } else {
+    navigateTo('/error');
+  }
 };
 
 window.onpopstate = () => {
-      console.log('hubo un cambio');
-      navigateTo(window.location.pathname);
-    };
-        
-navigateTo(window.location.pathname || defaultRoute)
+  console.log('hubo un cambio');
+  navigateTo(window.location.pathname);
+};
 
+navigateTo(window.location.pathname || defaultRoute);
