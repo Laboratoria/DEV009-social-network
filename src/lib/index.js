95d4fa7fd2527c12b77e1b35ab60b1775c0c449a
oolidu <arquitecturaod@gmail.com>
2023-08-03 23:26:39 -0400

    Pequeña mejora al css

--- src/lib/index.js
+++ src/lib/index.js
@@ -4,10 +4,21 @@ import { auth,createUserWithEmailAndPassword,updateProfile,db,collection,addDoc 
 
 
 
-const auth = getAuth(app);
+export const registerWithEmail = (email, password, displayName) => {
+  createUserWithEmailAndPassword(auth, email, password)
+  .then ((userCredential) => {
+  const uId = userCredential.user.uid;
+  saveUser({ userId:uId, Email:email, name:displayName })
+  updateProfile(userCredential.user, { displayName })
+      .then (() => userCredential)
 
+    })
+    .catch ((error) => {
+      console.log(error.message);
+    });
+};
 
-export const registrarUsuario = (email, pass) => {
+export const loginUsuario = (email, pass) => {
   try {
     createUserWithEmailAndPassword(auth, email, pass);
   } catch (error) {
