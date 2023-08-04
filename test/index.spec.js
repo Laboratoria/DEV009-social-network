b999f56c520ed9d66f76d4d809d7c1f416833fdc
Alba Giraldo <agiraldomolina@gmail.com>
2023-08-04 19:28:59 -0400

    With --env=jsdom working

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,20 +1,29 @@
 // importamos la funcion que vamos a testear
-// import { login } from '../src/components/login';
 import { loginUser, registerUser } from '../src/lib';
+import login from '../src/components/login';
+import register from '../src/components/register';
 
-describe('login', () => {
+describe('Section', () => {
+  it('Should display login component', () => {
+    const section = login();
+    console.log(section);
+  });
+});
+
+describe('Section resgister', () => {
+  it('Should display resgister component', () => {
+    const section = register();
+    console.log(section);
+  });
+});
+
+describe('loginUser', () => {
   it('Should show login page', () => {
     expect(typeof loginUser).toBe('function');
   });
 });
 
 describe('registeruser', () => {
-  it('Should show login page', () => {
-    expect(typeof registerUser).toBe('function');
-  });
-});
-
-describe('registeruserrrrr', () => {
   it('Should show login page', () => {
     expect(typeof registerUser).toBe('function');
   });
