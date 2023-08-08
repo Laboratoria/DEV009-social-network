672a245d4fbbe429147272251b22bef74ccd4e83
Nancy A Contreras <nancyalejandra24mv@gmail.com>
2023-08-08 16:24:50 -0500

    Se agrego el callback para que signup redireccione al muro

--- src/components/signup.js
+++ src/components/signup.js
@@ -4,27 +4,33 @@ function signup(navigateTo){
 const section = document.createElement('section');
 const logo = document.createElement('img');
 const title = document.createElement('h2');
-title.textContent = "Regístrate";
 
 const name = document.createElement('input');
 const formRegister = document.createElement('form');
 const email = document.createElement('input');
 const password = document.createElement('input');
-//const validatePassword = document.createElement('input');
 const btnSignup = document.createElement('button');
 const toLogin = document.createElement('p');
 const or = document.createElement('p');
 const signupWithGmail =  document.createElement('a');
 const linkGmail =  document.createElement('img');
 
+section.classList.add("conteinerSignup");
+logo.classList.add("logo");
+title.classList.add("title");
+name.classList.add("name");
+formRegister.classList.add("formRegister");
+
+
 logo.src = "./img/logo.png";
+title.textContent = "Regístrate";
 name.placeholder = "Nombre";
 email.placeholder = "ejemplo@correo.com";
 password.placeholder = "Contraseña";
 password.type= "password";
 password.setAttribute('required', '');
 password.setAttribute('pattern', '(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,}');
-//validatePassword.placeholder = "Confirmar contraseña";
+password.setAttribute('title', 'debe contener 8 o más caracteres que sean al menos un número y una letra mayúscula y minúscula');
 
 btnSignup.textContent = "Registrarse";
 toLogin.textContent = "Ya tengo una cuenta";
@@ -38,11 +44,20 @@ formRegister.addEventListener('submit',(e)=>{
     e.preventDefault();
     const emailUser = email.value;
     const passUser= password.value;
-    registerUser(emailUser, passUser);
+    const alerRegister = (boolean)=>{
+        if(boolean){
+            navigateTo('/wall');
+        }
+    }
+    registerUser(emailUser, passUser, alerRegister);
 })
 toLogin.addEventListener('click', ()=>{
     navigateTo("/");
 } )
+
+linkGmail.addEventListener('click', ()=>{
+    navigateTo('/signupGoogle');
+})
 
 section.append(logo, title, name, formRegister, toLogin, or, signupWithGmail, linkGmail);
 formRegister.append(email, password, btnSignup);
