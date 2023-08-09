b8d7ddeea494770d3bb004a33c1477aab26337b2
HanniaGC <hannia_gc@outlook.com>
2023-08-08 21:06:19 -0600

    base de datos

--- src/components/feed.js
+++ src/components/feed.js
@@ -1,4 +1,6 @@
+import { async } from 'regenerator-runtime';
 import { logoutUser } from '../lib';
+import { addRecipe } from '../lib/DataBase';
 
 function feed(navigateTo) {
   const section = document.createElement('section');
@@ -6,12 +8,15 @@ function feed(navigateTo) {
   const select = document.createElement('select');
   const option1 = document.createElement('option');
   const option2 = document.createElement('option');
-  const option3 = document.createElement('option');
   const write = document.createElement('button');
   const logo = document.createElement('img');
   const logoutButtom = document.createElement('button');
   const logoutMessage = document.createElement('p');
   const logoutMessageError = document.createElement('p');
+  const recipe = document.createElement ('textarea');
+  const formRecipe = document.createElement ('form');
+  const nameSteps = document.createElement('input');
+  const add = document.createElement('button');
 
   logo.src = './imagenes/image.png';
   write.textContent = 'Añade una Receta';
@@ -19,8 +24,11 @@ function feed(navigateTo) {
   option1.textContent = 'Mejores Recetas';
   option2.value = 'Usuarios';
   option2.textContent = 'Usuarios';
-  option3.value = 'salir';
-  option3.textContent = 'Salir';
+  recipe.placeholder= 'ingresa tu receta';
+  formRecipe.style.display = 'none'
+  nameSteps.type = 'text';
+  nameSteps.placeholder = 'Nombre de la receta';
+  add.textContent = 'Agregar';
   logoutButtom.textContent = 'Cerrar Sesión';
   logoutMessage.style.color = 'green';
   logoutMessageError.style.color = 'red';
@@ -28,10 +36,29 @@ function feed(navigateTo) {
     logoutUser();
     navigateTo('/');
   });
+  
+  write.addEventListener('click', () => {
+    formRecipe.style.display = 'block'
+  })
 
-  section.append(logo, logoutButtom, write, nav);
+  add.addEventListener('click', async (event) =>{
+  event.preventDefault();
+  const data = recipe.value;
+  const stepsRecipe = nameSteps.value;
+  console.log(data)
+  if (!stepsRecipe || !data){
+    console.log('Por favor, completa ambos campos.');
+    return;
+  }
+
+  await addRecipe(stepsRecipe, data);
+  console.log('Receta agregada exitosamente.');
+});
+
+  section.append(logo, formRecipe, write, nav, logoutButtom,);
+  formRecipe.append(nameSteps, recipe, add)
   nav.append(select);
-  select.append(option1, option2, option3);
+  select.append(option1, option2);
   return section;
 }
 export default feed;
