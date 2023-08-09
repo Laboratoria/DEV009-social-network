4bdcb7ca890a4a6446bdb87e0d3ba53f1f88f52c
oolidu <arquitecturaod@gmail.com>
2023-08-09 17:40:48 -0400

    commit definitivo de la tarde

--- src/components/registro.js
+++ src/components/registro.js
@@ -1,18 +1,29 @@
 import { registerWithEmail } from '../lib/index.js';
 
 function registro(navigateTo) {
-  const section = document.createElement('section'); /* section se usa en registro y en login */
+  const section = document.createElement('section');
+  section.className = 'section';
   const inputName = document.createElement('input');
+  inputName.className = 'inputName';
   const inputEmail = document.createElement('input');
+  inputName.className = 'inputEmail';
   const inputPass = document.createElement('input');
+  inputPass.className = 'inputPass';
   const buttonRegistro = document.createElement('button');
+  buttonRegistro.className = 'buttonRegistro';
   const buttonReturn = document.createElement('button');
+  buttonReturn.className = 'buttonReturn';
+  const errorRegister = document.createElement('h3');
 
   inputName.placeholder = 'Nombre de usuario';
+  inputName.className = 'displayName';
   inputEmail.placeholder = 'Correo';
   inputPass.placeholder = 'Contraseña';
 
-  buttonRegistro.textContent = 'registro';
+  buttonRegistro.textContent = 'Registro';
+  errorRegister.textContent = 'errorMessage';
+  errorRegister.style.display = 'none';
+  errorRegister.id = 'errorRegister';
 
   buttonRegistro.addEventListener('click', () => {
     const emailValue = inputEmail.value;
@@ -29,17 +40,46 @@ function registro(navigateTo) {
       userInfo.email,
       userInfo.password,
       userInfo.name,
-    );
+    )
+      .then((user) => {
+        navigateTo('/principal');
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        if (errorCode === 'auth/network-request-failed.') { /* Revisar codigo cuando los campos estan vacios */
+          errorRegister.style.display = 'block';
+          errorRegister.textContent = 'Los campos no pueden estar vacios';
+        } else if (errorCode === 'auth/weak-password') {
+          errorRegister.style.display = 'block';
+          errorRegister.textContent = 'La contraseña debe tener al menos 6 caracteres';
+        } else if (errorCode === 'auth/invalid-email') {
+          errorRegister.style.display = 'block';
+          errorRegister.textContent = 'Email invalido';
+        } else if (errorCode === 'auth/missing-email') {
+          errorRegister.style.display = 'block';
+          errorRegister.textContent = 'Falta colocar correo';
+        } else if (errorCode === 'auth/email-already-in-use') {
+          errorRegister.style.display = 'block';
+          errorRegister.textContent = 'El correo electrónico ya se encuentra registrado';
+        } else if (errorCode === 'auth/internal-error') {
+          errorRegister.style.display = 'block';
+          errorRegister.textContent = 'Falta colocar contraseña';
+        } else if (displayName === 'auth/invalid-display-name') { /* Falta mandar mensaje cuando el campo de usuario se encuentra vacio */
+          errorRegister.style.display = 'block';
+          errorRegister.textContent = 'Falta colocar usuario';
+        }
+        console.log(error.code);
+        return error;
+      });
   });
 
-  buttonReturn.textContent = 'back to home';
+  buttonReturn.textContent = 'Regresar';
   buttonReturn.addEventListener('click', () => {
     navigateTo('/');
   });
-  section.append(inputName, inputEmail, inputPass, buttonRegistro, buttonReturn);
+  section.append(inputName, inputEmail, inputPass, buttonRegistro, buttonReturn, errorRegister);
   return section;
 }
-
 //  navigateTo('/principal');
 
 export default registro;
