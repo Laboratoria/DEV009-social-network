43f871668abdeb205991f4c39265adce21929726
BrennDev <134910223+BrennDev@users.noreply.github.com>
2023-08-08 20:37:13 -0600

    Ignorar

--- src/components/registro.js
+++ src/components/registro.js
@@ -1,80 +1,77 @@
 import { registerWithEmail } from '../lib/index.js';
 
 function registro(navigateTo) {
-  const section = document.createElement('section'); /*section se usa en registro y en login*/
+  const section = document.createElement('section'); /* section se usa en registro y en login */
   const inputName = document.createElement('input');
   const inputEmail = document.createElement('input');
   const inputPass = document.createElement('input');
   const buttonRegistro = document.createElement('button');
   const buttonReturn = document.createElement('button');
-  const errorRegister = document.createElement('h3'); 
+  const errorRegister = document.createElement('h3');
 
   inputName.placeholder = 'Nombre de usuario';
-  inputName.className = 'displayName'
+  inputName.className = 'displayName';
   inputEmail.placeholder = 'Correo';
   inputPass.placeholder = 'Contraseña';
   buttonRegistro.textContent = 'registro';
   errorRegister.textContent = 'errorMessage';
   errorRegister.style.display = 'none';
-  errorRegister.id = 'errorRegister'
-  
+  errorRegister.id = 'errorRegister';
+
   buttonRegistro.addEventListener('click', () => {
     const emailValue = inputEmail.value;
     const nameValue = inputName.value;
     const passwordValue = inputPass.value;
-    
+
     const userInfo = {
       email: emailValue,
       name: nameValue,
       password: passwordValue,
     };
 
-    registerWithEmail(userInfo.email,
+    registerWithEmail(
+      userInfo.email,
       userInfo.password,
       userInfo.name,
-      )
-    .then((user)=>{
-      navigateTo('/principal');
-    })
-    .catch ((error) => {
-      const errorCode = error.code;
-      if (errorCode === 'auth/network-request-failed.'){ /*Revisar codigo cuando los campos estan vacios*/
-      errorRegister.style.display = 'block';
-      errorRegister.textContent = 'Los campos no pueden estar vacios' 
-      } else if (errorCode === 'auth/weak-password'){
-        errorRegister.style.display = 'block';
-        errorRegister.textContent = 'La contraseña debe tener al menos 6 caracteres' 
-      } else if (errorCode === 'auth/invalid-email'){
-        errorRegister.style.display = 'block';
-        errorRegister.textContent = 'Email invalido' 
-      } else if (errorCode === 'auth/missing-email'){
-        errorRegister.style.display = 'block';
-        errorRegister.textContent = 'Falta colocar correo' 
-      } else if (errorCode === 'auth/email-already-in-use'){
-        errorRegister.style.display = 'block';
-        errorRegister.textContent = 'El correo electrónico ya se encuentra registrado' 
-      } else if (errorCode === 'auth/internal-error'){
-        errorRegister.style.display = 'block';
-        errorRegister.textContent = 'Falta colocar contraseña' 
-      } else if (displayName === 'auth/invalid-display-name'){/*Falta mandar mensaje cuando el campo de usuario se encuentra vacio*/
-        errorRegister.style.display = 'block';
-        errorRegister.textContent = 'Falta colocar usuario' 
-      } 
-      console.log(error.code);
-      return error;
-    });
-    }) 
-        
-    buttonReturn.textContent = 'back to home';
-    buttonReturn.addEventListener('click', () => {
-      navigateTo('/');
-    });
-    section.append(errorRegister, inputName, inputEmail, inputPass, buttonRegistro, buttonReturn);
-    return section;
-  }
-  
-  //  navigateTo('/principal');
-  
+    )
+      .then((user) => {
+        navigateTo('/principal');
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        if (errorCode === 'auth/network-request-failed.') { /* Revisar codigo cuando los campos estan vacios */
+          errorRegister.style.display = 'block';
+          errorRegister.textContent = 'Los campos no pueden estar vacios';
+        } else if (errorCode === 'auth/weak-password') {
+          errorRegister.style.display = 'block';
+          errorRegister.textContent = 'La contraseña debe tener al menos 6 caracteres';
+        } else if (errorCode === 'auth/invalid-email') {
+          errorRegister.style.display = 'block';
+          errorRegister.textContent = 'Email invalido';
+        } else if (errorCode === 'auth/missing-email') {
+          errorRegister.style.display = 'block';
+          errorRegister.textContent = 'Falta colocar correo';
+        } else if (errorCode === 'auth/email-already-in-use') {
+          errorRegister.style.display = 'block';
+          errorRegister.textContent = 'El correo electrónico ya se encuentra registrado';
+        } else if (errorCode === 'auth/internal-error') {
+          errorRegister.style.display = 'block';
+          errorRegister.textContent = 'Falta colocar contraseña';
+        } else if (displayName === 'auth/invalid-display-name') { /* Falta mandar mensaje cuando el campo de usuario se encuentra vacio */
+          errorRegister.style.display = 'block';
+          errorRegister.textContent = 'Falta colocar usuario';
+        }
+        console.log(error.code);
+        return error;
+      });
+  });
 
+  buttonReturn.textContent = 'back to home';
+  buttonReturn.addEventListener('click', () => {
+    navigateTo('/');
+  });
+  section.append(errorRegister, inputName, inputEmail, inputPass, buttonRegistro, buttonReturn);
+  return section;
+}
 
 export default registro;
--- src/lib/initializeFirebase.js
+++ src/lib/initializeFirebase.js
@@ -1,8 +1,7 @@
-
-import { firebaseConfig } from './firebaseConfig.js';
 import { initializeApp } from 'firebase/app';
 import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
-import { getFirestore, collection,addDoc } from "firebase/firestore"
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
+import { firebaseConfig } from './firebaseConfig.js';
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
@@ -10,5 +9,7 @@ export const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-export {auth,createUserWithEmailAndPassword,updateProfile,db,collection,addDoc};
+export {
+  auth, createUserWithEmailAndPassword, updateProfile, db, collection, addDoc,
+};
 export const saveUser = (user) => addDoc(collection(db, 'Users'), user);
