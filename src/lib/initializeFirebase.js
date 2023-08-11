03a433e162bbfa05a04f9da96092f4c2b77b2a57
Natali <natalic1008.nm@gmail.com>
2023-08-10 21:04:39 -0500

    Registro con Google-parte1

--- src/lib/initializeFirebase.js
+++ src/lib/initializeFirebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, addDoc } from 'firebase/firestore';
-import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { getAuth, createUserWithEmailAndPassword, updateProfile, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { firebaseConfig } from './firebaseConfig.js';
 
 // Initialize Firebase
@@ -8,8 +8,9 @@ export const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 const db = getFirestore(app);
+const provider = new GoogleAuthProvider();
 
 export {
-  auth, createUserWithEmailAndPassword, updateProfile, db, collection, addDoc,
+  auth, createUserWithEmailAndPassword, updateProfile, db, collection, addDoc, provider, signInWithPopup,
 };
 export const saveUser = (user) => addDoc(collection(db, 'Users'), user);
