07f014494f8680a44a58a26256187f02325fea2f
Natali <natalic1008.nm@gmail.com>
2023-08-11 10:05:59 -0500

    Arreglo de problemas de identacion, funcion google ok

--- src/lib/initializeFirebase.js
+++ src/lib/initializeFirebase.js
@@ -1,6 +1,8 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, addDoc } from 'firebase/firestore';
-import { getAuth, createUserWithEmailAndPassword, updateProfile, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import {
+  getAuth, createUserWithEmailAndPassword, updateProfile, GoogleAuthProvider, signInWithPopup,
+} from 'firebase/auth';
 import { firebaseConfig } from './firebaseConfig.js';
 
 // Initialize Firebase
