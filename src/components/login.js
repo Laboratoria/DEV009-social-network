e31edabb63507198eb86c52e5a135ccdce1c372b
BrennDev <134910223+BrennDev@users.noreply.github.com>
2023-08-14 10:18:13 -0600

    Estilos CSS en Home, Registro y Login

--- src/components/login.js
+++ src/components/login.js
@@ -1,21 +1,62 @@
 function login(navigateTo) {
-  const section = document.createElement('section');
-  const buttonReturn = document.createElement('button');
+  const divLogin = document.createElement('divLogin');
+  divLogin.className = 'divLogin';
+
+  const logoBon = document.createElement('img');
+  logoBon.className = 'logoBon';
+
   const inputEmail = document.createElement('input');
+  inputEmail.className = 'input inputEmail';
+  inputEmail.setAttribute('type', 'email');
+  inputEmail.setAttribute('placeholder', 'Correo electronico');
+  inputEmail.setAttribute('required', '');
+
   const inputPass = document.createElement('input');
+  inputPass.className = 'input inputPass';
+  inputPass.setAttribute('type', 'password');
+  inputPass.setAttribute('placeholder', 'Crea tu contraseña');
+  inputPass.setAttribute('required', '');
+
   const buttonLogin = document.createElement('button');
+  buttonLogin.className = 'button buttonLogin';
+  buttonLogin.textContent = 'Inicia sesión';
 
-  inputEmail.placeholder = 'Escribe email';
-  inputPass.placeholder = 'Contraseña';
+  const buttonReturn = document.createElement('button');
+  buttonReturn.className = 'button buttonReturnLogin';
+  buttonReturn.textContent = 'Regresar';
 
-  buttonLogin.textContent = 'Login';
+  const errorRegister = document.createElement('p');
+  errorRegister.className = 'parrafo';
+  errorRegister.textContent = 'errorMessage';
+  errorRegister.style.display = 'none';
+  errorRegister.id = 'errorRegister';
 
-  buttonReturn.textContent = 'back to home';
+  const buttonGoogle = document.createElement('button');
+  buttonGoogle.className = 'button buttonGoogle';
+  const strong = document.createElement('strong');
+  strong.textContent = 'Google';
+  strong.className = 'textGoogle';
+  const imgGoogle = document.createElement('img');
+  imgGoogle.className = 'imgGoogle';
+
+  const textRegistrateCon = document.createElement('p');
+  textRegistrateCon.className = 'parrafo';
+  textRegistrateCon.textContent = 'O ingresa con...';
+
   buttonReturn.addEventListener('click', () => {
     navigateTo('/');
   });
-  section.append(inputEmail, inputPass, buttonLogin, buttonReturn);
-  return section;
+
+  divLogin.appendChild(logoBon);
+  divLogin.append(inputEmail, inputPass);
+  divLogin.appendChild(errorRegister);
+  divLogin.appendChild(buttonLogin);
+  divLogin.appendChild(textRegistrateCon);
+  divLogin.appendChild(buttonGoogle);
+  buttonGoogle.append(imgGoogle, strong);
+  divLogin.appendChild(buttonReturn);
+
+  return divLogin;
 }
 
 export default login;
