081e8b584004024f68a7d93e2ad70665745b28e2
Natali <natalic1008.nm@gmail.com>
2023-08-14 11:24:33 -0500

    Visualización de pagina de login de acuerdo a prototipo de alta fidelidad y funcion

--- src/lib/initializeFirebase.js
+++ src/lib/initializeFirebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, addDoc } from 'firebase/firestore';
 import {
-  getAuth, createUserWithEmailAndPassword, updateProfile, GoogleAuthProvider, signInWithPopup,
+  getAuth, createUserWithEmailAndPassword, updateProfile, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword
 } from 'firebase/auth';
 import { firebaseConfig } from './firebaseConfig.js';
 
@@ -13,6 +13,6 @@ const db = getFirestore(app);
 const provider = new GoogleAuthProvider();
 
 export {
-  auth, createUserWithEmailAndPassword, updateProfile, db, collection, addDoc, provider, signInWithPopup,
+  auth, createUserWithEmailAndPassword, updateProfile, db, collection, addDoc, provider, signInWithPopup, signInWithEmailAndPassword
 };
 export const saveUser = (user) => addDoc(collection(db, 'Users'), user);
