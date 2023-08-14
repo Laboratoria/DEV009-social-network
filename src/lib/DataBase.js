e8d4821b2be4ced2d8705a205455feb6a58f1b9c
HanniaGC <hannia_gc@outlook.com>
2023-08-14 10:43:02 -0600

    Se guarda los datos de la receta y ya los podemos ver en consola

--- src/lib/DataBase.js
+++ src/lib/DataBase.js
@@ -1,5 +1,5 @@
 
-import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { getFirestore, collection, addDoc,  doc, getDoc } from "firebase/firestore";
 import { app } from './firebase.js';
 
 export const db = getFirestore(app);
@@ -10,9 +10,29 @@ export async function addRecipe(name, steps) {
       name: name,
       Pasos: steps
     });
-
     console.log("Recipe added with ID: ", docRef.id);
+    return docRef.id;
   } catch (error) {
     console.error("Error adding recipe: ", error);
   }
+}
+
+export async function  fetchRecipe(recipeId){
+  const recipeRef = doc(db, "recetas", recipeId);
+  try{
+  const docSnap = await getDoc(recipeRef);
+
+  if (docSnap.exists()) {
+    const recipeData = docSnap.data();
+    console.log("Document data:", recipeData);
+    return recipeData;
+  } else {
+    // docSnap.data() will be undefined in this case
+    console.log("No such document!");
+    return null;
+  }
+}catch (error) {
+  console.error("Error o se encuentra la receta: ", error);
+    return null;
+}
 }
