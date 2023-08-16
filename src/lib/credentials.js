e2adea00525bf9548d8dcbafb9a7e5c710c0ef61
Andrea <a.aparicio.g@hotmail.com>
2023-08-15 18:38:21 -0600

    Se configuro e inicializo realtime Database, se creo la funcion writeUserData y se modifico registerUser para poder almacenar la informacion cuando se registre un usuario nuevo

--- src/lib/credentials.js
+++ src/lib/credentials.js
@@ -1,17 +1,31 @@
 // Import the functions you need from the SDKs you need
-
 import {
   auth, provider, createUserWithEmailAndPassword, signInWithPopup, getRedirectResult,
-  GoogleAuthProvider, signInWithRedirect,
+  GoogleAuthProvider, signInWithRedirect, database, set, ref,
 } from './initializerFirebase.js';
 
-export const registerUser = (email, password) => {
+function writeUserData(userId, Name, lastName, userName) {
+  const db = database;
+  set(ref(db, `users/${userId}`), {
+    username: userName,
+    name: Name,
+    lastname: lastName,
+  });
+}
+
+function registerUser(email, password, name, lastname, userName) {
   try {
-    createUserWithEmailAndPassword(auth, email, password);
+    createUserWithEmailAndPassword(
+      auth,
+      email,
+      password,
+    ).then((userId) => {
+      writeUserData(userId.user.uid, name, lastname, userName);
+    });
   } catch (error) {
     throw error.message;
   }
-};
+}
 
 export const signWithGoogle = () => {
   try {
@@ -45,6 +59,11 @@ export const signIn = () => {
   } catch (error) {
     throw error.message;
   }
+};
+
+export {
+  registerUser,
+  writeUserData,
 };
 // export const redirectGoogle = () => {
 //   try {
--- src/lib/initializerFirebase.js
+++ src/lib/initializerFirebase.js
@@ -3,13 +3,16 @@ import {
   getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, getRedirectResult,
   signInWithRedirect,
 } from 'firebase/auth';
+import { getDatabase, ref, set } from 'firebase/database';
 import { firebaseConfig } from './firebaseConfig.js';
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
+// Initialize Realtime Database and get a reference to the service
+export const database = getDatabase(app);
 
 export {
   createUserWithEmailAndPassword, signInWithPopup, getRedirectResult, GoogleAuthProvider,
-  signInWithRedirect,
+  signInWithRedirect, ref, set, getDatabase, 
 };
