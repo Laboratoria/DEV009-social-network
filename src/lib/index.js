edb1dc4d013ce061a008f3d4a1944c3d0f2dae70
KellPitt19 <kelly.g.pitter@gmail.com>
2023-08-17 10:19:36 -0400

    se agraga funcionalidades login y logout

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,50 +1,75 @@
 // aqui exportaras las funciones que necesites
 
+import { async } from 'regenerator-runtime';
 import {
-  createUserWithEmailAndPassword, auth, provider, signInWithPopup, GoogleAuthProvider, signInWithEmailAndPassword,
+  createUserWithEmailAndPassword, auth, provider, signInWithPopup, GoogleAuthProvider, signInWithEmailAndPassword, signOut,
 } from '../firebase/initializeFirebase.js';
 
 // ----                   signin with new email                    --- //
-export const registerUser = (email, pass, callback) => {
-  createUserWithEmailAndPassword(auth, email, pass)
-    .then((userCredential) => {
-      const user = userCredential.user;
-      callback(true);
-    })
-    .catch((error) => {
-      console.log(error.code);
-      if (error.code === 'auth/email-already-in-use') {
-        alert('Este email ya está registrado');
-    } else if (error.code === 'auth/invalid-email') {
-        alert('Este email es inválido');
-      }
-      callback(false);
-    });
+export const registerUser = async (email, pass, callback) => {
+  
+  try {
+    await createUserWithEmailAndPassword(auth, email, pass)
+    callback(true);
+
+  } catch (error) {
+   console.log (error.code);
+  if (error.code === "auth/email-already-in-use"){
+    alert("Este email ya está registrado");
+  }else if (error.code === "auth/invalid-email"){
+    alert("Este email es inválido");
+  } 
+  callback(false); 
+  }
 };
 
 // ----                   signin with google                    --- //
-export const registerWithGoogle = (callback) => {
-  signInWithPopup(auth, provider)
-    .then((result) => {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      // The signed-in user info.
-      const user = result.user;
-      // IdP data available using getAdditionalUserInfo(result)
-      // ...
-      callback(true);
-    }).catch((error) => {
-    // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.email;
-      // The AuthCredential type that was used.
-      const credential = GoogleAuthProvider.credentialFromError(error);
-      // ...
-      callback(false);
-    });
+export const registerWithGoogle = async (callback) =>{
+try {
+  const result = await signInWithPopup(auth, provider)
+
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  const credential = GoogleAuthProvider.credentialFromResult(result);
+  const token = credential.accessToken;
+  // The signed-in user info.
+  const user = result.user;
+  // IdP data available using getAdditionalUserInfo(result)
+  // ...
+  callback(true);
+} catch (error) {
+   // Handle Errors here.
+   const errorCode = error.code;
+   const errorMessage = error.message;
+   // The email of the user's account used.
+   const email = error.email;
+   // The AuthCredential type that was used.
+   const credential = GoogleAuthProvider.credentialFromError(error);
+   // ...
+   callback(false);
+}
 };
 
 // ----                   login                   --- //
+export const loginUser = async (email, pass, callback) => {
+  await signInWithEmailAndPassword(auth, email, pass)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+    callback(true);
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    errorCode = alert("Usuario o contraseña incorrecta");
+    callback(false);
+  });
+}
+
+export const exitUser = async () => {
+  await signOut(auth).then(() => {
+    // Sign-out successful.
+  }).catch((error) => {
+    console.log ("error")
+    // An error happened.
+  });
+}
