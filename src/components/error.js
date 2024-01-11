2fe692d873b91dde79a07d64f311dd723327d6a5
Andrea Cabrera Robles <marisoljbnick@hotmail.com>
2024-01-11 17:02:03 -0600

    Correcciones finales en estilo

--- src/components/error.js
+++ src/components/error.js
@@ -1,12 +1,13 @@
 function error(navigateTo) {
-  const section = document.createElement('section');
+  const section = document.createElement('div');
   const title = document.createElement('h2');
-  title.textContent = 'Error 404 página no encontrada';
   const btnError = document.createElement('button');
+
   btnError.classList.add('btnError');
   title.classList.add('error');
   section.classList.add('errorSection');
 
+  title.textContent = 'Error 404: página no encontrada';
   btnError.textContent = 'Regresar a inicio';
 
   btnError.addEventListener('click', () => {
